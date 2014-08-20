45642d1cebe19dbb5eddd86a1adb2f43fce01f3f
Sinevia Ltd <info@sinevia.com>
2014-08-20 04:50:39 +0100

    Fixing bug

--- files/run_site.sh
+++ files/run_site.sh
@@ -2,7 +2,7 @@
 echo "Showing the container's ip:"
 ip addr show | grep 'eth0'
 echo "Starting sshd:"
-/usr/sbin/sshd -D
+/usr/sbin/sshd
 echo "Starting node.js:"
-/site/node_modules/.bin/supervisor /site/app.js
+/site/node_modules/.bin/supervisor --watch /site --extensions 'node,js,html'  /site/app.js
 echo "Ready!"
